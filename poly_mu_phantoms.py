2cef9114d1a53c93cde6f462fbd01f6acff091f3
gogbajbobo <grigoriev.maxim@gmail.com>
2023-09-28 11:46:55 +0300

    Update poly_mu_phantoms.py

--- poly_mu_phantoms.py
+++ poly_mu_phantoms.py
@@ -17,6 +17,8 @@
 import xraydb
 import spekpy
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
 import numpy as np
 
 from skimage.filters import threshold_otsu, gaussian, median
@@ -29,7 +31,14 @@ s.filter('Air', 1440)
 spek_energies, spek_intensities = s.get_spectrum()
 spek_intensities /= spek_intensities.sum()
 
+s = spekpy.Spek(kvp=50, dk=0.5, targ='Mo')
+s.filter('Air', 1440)
+s.filter('Al', 2)
+_, spek_intensities_Al_filter = s.get_spectrum()
+spek_intensities_Al_filter /= spek_intensities_Al_filter.sum()
+
 plt.plot(spek_energies, spek_intensities)
+plt.plot(spek_energies, spek_intensities_Al_filter)
 plt.yscale('log')
 plt.ylim(1e-4, 1e0)
 plt.grid()
@@ -59,8 +68,12 @@ plt.show()
 spek_intensities_eff = spek_intensities * GOS_eff_50
 spek_intensities_eff /= spek_intensities_eff.sum()
 
+spek_intensities_eff_Al_filter = spek_intensities_Al_filter * GOS_eff_50
+spek_intensities_eff_Al_filter /= spek_intensities_eff_Al_filter.sum()
+
 plt.plot(spek_energies, spek_intensities)
 plt.plot(spek_energies, spek_intensities_eff)
+plt.plot(spek_energies, spek_intensities_eff_Al_filter)
 plt.yscale('log')
 plt.ylim(1e-4, 1e0)
 plt.grid()
@@ -80,41 +93,42 @@ plt.show()
 # %%
 def calc_object_mus_from_spectrum(im, spectrum, mat_att, voxel_size):
 
-    angles = np.arange(0, 180, 1)
+    angles = np.arange(0, 360, 1)
     sino_shape = (angles.size, im.shape[0])
-    sinogram_wo_GOS = np.zeros(sino_shape)
+    sinogram = np.zeros(sino_shape)
     
     for j, angle in enumerate(angles):
         
-        r_im = rotate(im, angle, order=1, preserve_range=True)
+        # r_im = rotate(im, angle, order=1, preserve_range=True)
+        r_im = rotate(im, angle, order=0, preserve_range=True)
         ray_sums = np.sum(r_im, axis=1) * voxel_size
         trans_sums = np.exp(np.outer(-mat_att, ray_sums)).T
         
-        passed_specs_wo_GOS = trans_sums * spectrum
-        passed_intensity_wo_GOS = np.sum(passed_specs_wo_GOS, axis=1)
-        sinogram_wo_GOS[j] = -np.log(passed_intensity_wo_GOS)
+        passed_specs = trans_sums * spectrum
+        passed_intensity = np.sum(passed_specs, axis=1)
+        sinogram[j] = -np.log(passed_intensity)
         
-    recon_wo_GOS = gaussian(iradon(sinogram_wo_GOS.T, theta=angles).T)
-    recon_wo_GOS[im == 0] = 0
-    recon_wo_GOS[recon_wo_GOS < 0] = 0
-    recon_wo_GOS /= voxel_size # convert to 1/mm values
+    recon = gaussian(iradon(sinogram.T, theta=angles).T)
+    recon[im == 0] = 0
+    recon[recon < 0] = 0
+    recon /= voxel_size # convert to 1/mm values
     
-    plt.imshow(recon_wo_GOS)
+    plt.imshow(recon)
     plt.colorbar()
     plt.show()
     
     row = im.shape[0] // 2
     plt.figure(figsize=(10, 5))
-    plt.plot(recon_wo_GOS[row])
+    plt.plot(recon[row])
     plt.ylabel('Коэффициент ослабления, 1/мм')
     plt.grid(color='gray')
     plt.show()
     
-    return recon_wo_GOS
+    return recon
 
 
 # %%
-size = 256
+size = 257
 shape = (size, size)
 im1 = np.zeros(shape).astype(int)
 rr, cc = disk((size//2, size//2), size//3, shape=shape)
@@ -125,15 +139,14 @@ plt.imshow(im1)
 # %%
 voxel_size = 0.009 # in mm — 0.01 = 10µm
 
-_ = calc_object_mus_from_spectrum(im1, spek_intensities_eff, att_SiC, voxel_size)
+recon_0 = calc_object_mus_from_spectrum(im1, spek_intensities_eff, att_SiC, voxel_size)
 
 # %%
-size = 256
-shape = (size, size)
 im2 = np.zeros(shape).astype(int)
 rr, cc = disk((size//2, size//2), size//3, shape=shape)
 im2[rr, cc] = 1
 
+# rr, cc = disk((size//3, size//2), size//8, shape=shape)
 rr, cc = disk((size//3, size//2), size//12, shape=shape)
 im2[rr, cc] = 2
 
@@ -143,77 +156,68 @@ im2[rr, cc] = 2
 plt.imshow(im2)
 
 # %%
-np.unique(im1, return_counts=True)
-
-# %%
-np.unique(im2, return_counts=True)
-
-# %%
-rim = rotate(im2, 10, order=0, preserve_range=True)
-np.unique(rim, return_counts=True)
-
-# %%
-counts = np.apply_along_axis(np.bincount, 1, im2, minlength=np.max(im2)+1)
-counts.shape
-
-# %%
-plt.imshow(counts * voxel_size)
-
-# %%
 attenuations = np.array([att_SiC, att_Fe])
+# attenuations = np.array([att_SiC, att_SiC])
 attenuations.shape
 
-# %%
-counts[:, 1:][106]
 
 # %%
-np.outer(-attenuations, counts[:, 1:]).shape
+def recon_multiple_matters(im, attenuations, spectrum, voxel_size):
 
-# %%
-
-# %%
-
-# %%
-im = np.copy(im2).astype(int)
-
-angles = np.arange(0, 180, 1)
-sino_shape = (angles.size, im.shape[0])
-sinogram = np.zeros(sino_shape)
-
-counts_minlength=np.max(im) + 1
-
-for j, angle in enumerate(angles):
+    angles = np.arange(0, 360, 1)
+    sino_shape = (angles.size, im.shape[0])
+    sinogram = np.zeros(sino_shape)
     
-    r_im = rotate(im, angle, order=0, preserve_range=True)
-    ray_sums = np.apply_along_axis(np.bincount, 1, r_im, minlength=counts_minlength) * voxel_size
-    trans_sums = np.exp(np.outer(-mat_att, ray_sums)).T
+    counts_minlength=np.max(im) + 1
     
-    passed_specs = trans_sums * spectrum
-    passed_intensity = np.sum(passed_specs, axis=1)
-    sinogram[j] = -np.log(passed_intensity)
+    for j, angle in enumerate(angles):
+        
+        r_im = rotate(im, angle, order=0, preserve_range=True)
+        ray_sums = np.apply_along_axis(np.bincount, 1, r_im, minlength=counts_minlength)[:, 1:].T * voxel_size
     
-recon = gaussian(iradon(sinogram.T, theta=angles).T)
-recon[im == 0] = 0
-recon[recon < 0] = 0
-recon /= voxel_size # convert to 1/mm values
+        att_material_map = np.zeros((attenuations.shape[1], ray_sums.shape[1]))
+        for idx, _ in enumerate(ray_sums):
+            att_material_map += np.outer(attenuations[idx], ray_sums[idx])
+            
+        trans_sums = np.exp(-att_material_map).T
+        
+        passed_specs = trans_sums * spectrum
+        passed_intensity = np.sum(passed_specs, axis=1)
+        sinogram[j] = -np.log(passed_intensity)
+        
+    recon = gaussian(iradon(sinogram.T, theta=angles).T)
+    recon[im == 0] = 0
+    recon[recon < 0] = 0
+    recon /= voxel_size # convert to 1/mm values
 
-plt.imshow(recon)
-plt.colorbar()
+    return recon
+
+
+# %%
+recon_1 = recon_multiple_matters(im2, attenuations, spek_intensities_eff, voxel_size)
+recon_2 = recon_multiple_matters(im1, attenuations, spek_intensities_eff_Al_filter, voxel_size)
+recon_3 = recon_multiple_matters(im2, attenuations, spek_intensities_eff_Al_filter, voxel_size)
+
+# %%
+fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+im_0 = ax[0].imshow(recon_1)
+plt.colorbar(im_0, ax=ax[0])
+im_1 = ax[1].imshow(recon_2)
+plt.colorbar(im_1, ax=ax[1])
+im_2 = ax[2].imshow(recon_3)
+plt.colorbar(im_2, ax=ax[2])
 plt.show()
 
 row = im.shape[0] // 2
 plt.figure(figsize=(10, 5))
-plt.plot(recon[row])
+plt.plot(recon_0[row])
+plt.plot(recon_1[row])
+plt.plot(recon_2[row])
+plt.plot(recon_3[row])
 plt.ylabel('Коэффициент ослабления, 1/мм')
 plt.grid(color='gray')
 plt.show()
 
-
-# %%
-
-# %%
-
-# %%
 
 # %%
 
