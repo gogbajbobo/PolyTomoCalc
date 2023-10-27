48701874ceb71b83a2c8ed65ebf0a092c6a3a55b
gogbajbobo <grigoriev.maxim@gmail.com>
2023-10-27 10:59:45 +0300

    Update poly_point.py

--- poly_point.py
+++ poly_point.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.15.1
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -14,38 +14,21 @@
 # ---
 
 # %%
+import spekpy
 import xraydb
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_otsu, gaussian, median
 
-
 # %%
-def extract_data(file_name):
-    res = []
-    with open(file_name,'rb') as f:
-        find_data = False
-        for l in f.readlines():
-            s = l.decode('ascii').replace('\r\n','')
-            if s == '<<END>>'  and find_data:
-                break
-            if find_data:
-                res.append(s)
-            if s == '<<DATA>>':
-                find_data =True
+input_path = 'Mo_spec_poly_45.npy'
+with open(input_path, 'rb') as f:
+    spectrum = np.load(f).astype(float)
 
-        return np.array(res).astype(int)
-
-
-# %%
-spectrum = extract_data('/Users/grimax/Desktop/tmp/Iohexol_samples/20230829 -- Mo-tube-poly-45kV .mca')
-plt.plot(spectrum)
-plt.yscale('log')
-
-# %%
-en_step = (19.608 - 17.479) / (416 - 371)
-en_keV = np.array([17.479 + (i - 371) * en_step for i in np.arange(spectrum.shape[0])])
+input_path = 'Mo_spec_poly_45_energies.npy'
+with open(input_path, 'rb') as f:
+    en_keV = np.load(f).astype(float)
 
 plt.plot(en_keV, spectrum)
 plt.yscale('log')
@@ -101,7 +84,7 @@ plt.show()
 print(iohexol_mu[371])
 
 # %%
-input_path = '/Users/grimax/Desktop/tmp/Iohexol_samples/dbace4ca-3ba6-4a8a-b191-d52fe70c8a4f.npy'
+input_path = '/Users/grimax/Documents/Science/xtomo/poly_tomo_calc/Iohexol_samples/dbace4ca-3ba6-4a8a-b191-d52fe70c8a4f.npy'
 with open(input_path, 'rb') as f:
   im = np.load(f)
 
@@ -195,12 +178,12 @@ print(f'recon point sum: {recon_point_sum}')
 
 
 # %%
-def point_sum_calc(spectrum_filtered):
+def point_sum_calc(spectrum_filtered, ihx_mu):
 
     voxel_size = 0.009 # in mm — 0.01 = 10µm
 
     ray_sum = bim[line_y].sum() * voxel_size
-    trans_sum = np.exp(-iohexol_mu * ray_sum)
+    trans_sum = np.exp(-ihx_mu * ray_sum)
     spectrum_passed = spectrum_filtered * trans_sum
 
     sf_GOS = spectrum_filtered * GOS_eff
@@ -218,7 +201,7 @@ def point_sum_calc(spectrum_filtered):
 
 
 # %%
-point_sum_calc(spectrum_filtered)
+point_sum_calc(spectrum_filtered, iohexol_mu)
 
 # %%
 from _handler_funcs import generate_spectrum
@@ -238,9 +221,57 @@ plt.ylim([1e-6, 5e-1])
 plt.yscale('log')
 
 # %%
-point_sum_calc(spectrum_filtered)
+point_sum_calc(spectrum_filtered, iohexol_mu)
 
 # %%
-point_sum_calc(sf)
+point_sum_calc(sf, iohexol_mu)
+
+# %%
+en_step = np.mean(en_keV[1:] - en_keV[:-1])
+
+s = spekpy.Spek(kvp=45, dk=en_step, targ='Mo')
+s.filter('Air', 1440)
+energies, intensities = s.get_spectrum()
+intensities /= intensities.sum()
+
+plt.plot(energies, intensities)
+
+plt.ylim([2e-5, 4e-1])
+plt.yscale('log')
+plt.grid()
+
+# %%
+GOS_mus = xraydb.material_mu('GOS', energies * 1000) / 10
+GOS_t = np.exp(-GOS_mus * 22 * 0.001) # (22 * 0.001)mm == 22µm
+# GOS_t = np.exp(-GOS_mus * 11 * 0.001)
+beta = 3
+en_gap = 4.6 # eV
+
+GOS_n_p = energies * 1000 / (beta * en_gap)
+GOS_eff = GOS_n_p * (1 - GOS_t)
+
+plt.plot(energies, GOS_eff)
+plt.grid()
+plt.show()
+
+
+# %%
+iohexol_mu = xraydb.material_mu('iohexol', energies * 1000) / 10
+plt.plot(energies, iohexol_mu)
+plt.yscale('log')
+plt.show()
+
+
+# %%
+point_sum_calc(intensities, iohexol_mu)
+
+# %%
+a = np.array([[2, 2, 1]])
+b = np.array([[45, 4, 1]])
+t = np.array([[1, 2, 3], [3, 4, 5], [6, 3, 1]])
+np.matmul(a.T, b)
+
+# %%
+np.linalg.inv(b.T)
 
 # %%
