3267bf72edef8ff9fd3a3337643e14760d2c6c58
gogbajbobo <grigoriev.maxim@gmail.com>
2023-10-27 10:59:47 +0300

    Update test_spekpy.py

--- test_spekpy.py
+++ test_spekpy.py
@@ -24,16 +24,20 @@ from _handler_funcs import generate_spectrum
 en_step = (19.608 - 17.479) / (416 - 371)
 
 s = spekpy.Spek(kvp=45, dk=en_step, targ='Mo')
-s.filter('Air', 1440)
 energies, intensities = s.get_spectrum()
 intensities /= intensities.sum()
 
+s.filter('Air', 1440)
+_, intensities_with_air = s.get_spectrum()
+intensities_with_air /= intensities_with_air.sum()
+
+att_air = np.exp(-xraydb.material_mu('air', energies * 1000) * 144)
+intensities_with_air_2 = intensities * att_air
+intensities_with_air_2 /= intensities_with_air_2.sum()
+
 plt.plot(energies, intensities)
-
-energies_1, intensities_1 = s.get_spectrum(flu=False)
-intensities_1 /= intensities_1.sum()
-
-plt.plot(energies_1, intensities_1)
+plt.plot(energies, intensities_with_air)
+plt.plot(energies, intensities_with_air_2)
 
 plt.ylim([2e-5, 4e-1])
 plt.yscale('log')
@@ -48,17 +52,10 @@ sf = s * att_air
 sf /= sf.sum()
 
 plt.plot(energies, sf)
+plt.plot(energies, intensities_with_air)
 plt.grid()
 plt.ylim([1e-6, 5e-1])
 plt.yscale('log')
-
-# %%
-plt.plot(energies, intensities)
-plt.plot(energies_1, intensities_1)
-plt.plot(energies, sf)
-plt.ylim([2e-5, 4e-1])
-plt.yscale('log')
-plt.grid()
 
 # %%
 input_path = 'Mo_spec_poly_45.npy'
@@ -77,20 +74,33 @@ spec_Mo_45_energies_1 = spec_Mo_45_energies[idx_min:idx_max]
 spec_Mo_45_1 = spec_Mo_45[idx_min:idx_max]
 spec_Mo_45_1 /= spec_Mo_45_1.sum()
 
-spec_Mo_45_2 = spec_Mo_45_1 * spec_Mo_45_energies_1
-spec_Mo_45_2 /= spec_Mo_45_2.sum()
-
 # %%
-plt.plot(energies, intensities)
-plt.plot(energies_1, intensities_1)
+plt.plot(energies, intensities_with_air)
 plt.plot(energies, sf)
 plt.plot(spec_Mo_45_energies_1, spec_Mo_45_1)
-plt.plot(spec_Mo_45_energies_1, spec_Mo_45_2)
 plt.ylim([2e-5, 4e-1])
 plt.yscale('log')
 plt.grid()
 
 # %%
 whos
+
+# %%
+s = spekpy.Spek(kvp=40, dk=0.25, targ='Mo')
+energies, intensities = s.get_spectrum()
+intensities /= intensities.sum()
+
+s.filter('Air', 1440)
+_, intensities_with_air = s.get_spectrum()
+intensities_with_air /= intensities_with_air.sum()
+
+plt.figure(figsize=(10, 5))
+
+# plt.plot(energies, intensities)
+plt.plot(energies, intensities_with_air)
+
+plt.ylim([2e-5, 4e-1])
+plt.yscale('log')
+plt.grid()
 
 # %%
